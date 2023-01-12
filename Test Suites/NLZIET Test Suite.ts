690c5173175d6f076b10c4bc4d70d5fb9b7877dc
frankvanschaik <frankvanschaik@franks-mbp>
2023-01-12 19:47:06 +0100

    Test Suite

---
+++ Test Suites/NLZIET Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NLZIET Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b4eea743-b1cb-4ec0-a61b-8cfaebd084e0</testSuiteGuid>
+</TestSuiteEntity>
